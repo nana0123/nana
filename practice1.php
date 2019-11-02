6ab4563429d53c366c5ea4a74ecdd670bc833888
”名前” <>
2019-11-02 14:14:51 +0000

    PHP03

--- practice1.php
+++ practice1.php
@@ -1,5 +1,89 @@
 <?php
+$height = 160;
+if ($height < 150) {
+  echo "150cm 未満の方はご乗車できません。";
+}
+echo "\n";
+if ($height < 150) {
+  echo "150cm 未満の方はご乗車できません。";
+} else {
+  echo "ご乗車になれます。";
+}
+$height = 230;
+echo "\n";
+if ($height < 150) {
+  echo "150cm 未満の方はご乗車できません。";
+} else if ($height >= 200){
+  echo "200cm 以上の方はご乗車できません。";
+}else {
+  echo "ご乗車になれます。";
+}
+$weekday = "月曜";
+echo "\n";
+switch ($weekday) {
+  case "月曜":
+    echo "可燃ごみの日です。";
+    break;
+  case "水曜":
+    echo "資源ごみの日です。";
+    break;
+  default:
+    echo "回収はありません。";
+    break;
+}
+$weekday = "月曜";
+echo "\n";
+switch ($weekday) {
+  case "月曜":
+  case "木曜":
+    echo "可燃ごみの日です。";
+    break;
+  case "水曜":
+    echo "資源ごみの日です。";
+    break;
+  default:
+    echo "回収はありません。";
+    break;
+}
+$weekday = "木曜";
+switch ($weekday) {
+  case "月曜":
+  case "木曜":
+    echo "可燃ごみの日です。";
+    break;
+  case "水曜":
+    echo "資源ごみの日です。";
+    break;
+  default:
+    echo "回収はありません。";
+    break;
+}
+echo "\n";
 $a = 3;
-$b = 7;
-$new_a = $a + 7;
-echo $new_a;
+$b = 3;
+$c = "3";
+var_dump($a == $b);
+var_dump($a != $b);
+var_dump($a > $b);
+var_dump($a >= $b);
+var_dump($a < $b);
+var_dump($a <= $b);
+var_dump($a === $c);
+var_dump($a !== $c);
+for($i = 0; $i < 10; $i++){
+  echo $i;
+}
+$total = 0;
+echo $total;
+for ($i = 0; $i <= 100; $i++) {
+  $total += $i;
+}
+echo $total;
+$fruits = array("apple", "orange", "lemon");
+$animals = array("dog", "cat", "panda");
+foreach($animals as $animal){
+  echo "要素は" . $animal;
+  echo "\n";
+}
+echo "\n";
+
---
+++ practice2.php
@@ -0,0 +1,25 @@
+<?php
+$name = "Yoshiko";
+if ($name == "Yoshiko") {
+  echo "私は Yoshiko です";
+} else if ($name != "Yoshiko"){
+  echo "Yoshikoではありません。";
+}
+echo "\n";
+for ($i = 1; $i <= 10000; $i++) {
+  $total += $i;
+}
+echo $total;
+echo "\n";
+$fruits = array("apple", "orange", "melon","mango","Strawberry");
+foreach($fruits as $fruits){
+  echo $fruits;
+  echo "\n";
+}
+$start = 1;
+$end = 100;
+for($i = 1; $i <= 100; $i++)
+if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+}
